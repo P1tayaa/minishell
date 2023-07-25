132499b2fc4261b0f4f845b814b792d716ee838e
Samuel Boulain <sboulain@f1r6s17.codam.nl>
2023-07-25 12:31:50 +0200

    finish polish parser

--- parser.c
+++ parser.c
@@ -6,7 +6,7 @@
 /*   By: sboulain <sboulain@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 13:30:47 by sboulain          #+#    #+#             */
-/*   Updated: 2023/07/22 17:29:34 by sboulain         ###   ########.fr       */
+/*   Updated: 2023/07/25 10:55:22 by sboulain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,29 +27,31 @@
 
 typedef struct s_lexer
 {
-    char tokenid[3];
-	char *content;
-    char *cmd;
-    char *flags;
-    char *args;
-    // char *content;
-    int    possition;
-    
-}    t_lexer;
+	char	tokenid[3];
+	char	*content;
+	char	*cmd;
+	char	*flags;
+	char	*args;
+	int		possition;
+
+}	t_lexer;
 
 int		count_pipes(char *str);
 char	**get_list_of_tokenid(void);
 void	free_list_of_tokenid(char **list_of_tokenid);
 int		count_token(char *input);
-int	copy_input_to_parcer_and_keep_track_of_curser(char *input, t_lexer *current_lex, int curser);
+// int	copy_input_to_parcer_and_keep_track_of_curser(char *input, t_lexer *current_lex, int curser);
 void	free_parser(t_lexer **lexer);
-
-int parse_until_token_id(char *input, int current_lex, t_lexer **lexer, int curser);
+void	free_lexer(t_lexer **lexer);
+int 	parse_until_token_id(char *input, int current_lex, t_lexer **lexer, int curser);
 
 
 // figure out where ls -la | grep "." | cat > out -e > out2
 // the -e should be
+/*
+	so it take the user input (string) and parse it into out lexer struct
 
+*/
 t_data_table	*parsse_things(char *str)
 {
 	int	token_num;
@@ -65,54 +67,56 @@ t_data_table	*parsse_things(char *str)
 	curser = 0;
 	while (i <= token_num)
 	{
-		printf("mallocing tokennum %d\n", i);
 		lexer[i] = (t_lexer *)malloc(sizeof(t_lexer));
+		if (!lexer[i])
+			exit(1);
 		lexer[i]->possition = i;
-		// lexer[i]->args = NULL;
-		// lexer[i]->cmd = NULL;
+		lexer[i]->args = NULL;
+		lexer[i]->cmd = NULL;
 		lexer[i]->flags = NULL;
 		lexer[i]->tokenid[0] = '\0';
-		// curser = copy_input_to_parcer_and_keep_track_of_curser(str, lexer[i], curser);
 		curser = parse_until_token_id(str, i, lexer, curser);
-		// if (i == 0)
-		// 	lexer[i]->tokenid[0] = '\0';
-		// else
 			
 		i++;
 	}
 	lexer[i] = NULL;
-	// printf("lexer[i] == %p\ni == %d, token_num == %d\n", lexer[i], i, token_num);
 	i = 0;
 	while (lexer[i] != NULL)
 	{
 		puts("test start reading lexers");
-		printf("flags: \"%s\", ",lexer[i]->flags);
-		printf("args: \"%s\", ",lexer[i]->args);
 		printf("possition %d, ",  lexer[i]->possition);
 		printf("cmd: \"%s\", ", lexer[i]->cmd);
+		printf("flags: \"%s\", ",lexer[i]->flags);
+		printf("args: \"%s\", ",lexer[i]->args);
 		printf("tokenID: \"%s\"\n",lexer[i]->tokenid);
 		i++;
 	}
-	// puts("freeur");
-	// free_parser(lexer);
-	// puts("end freeur");
-	
+	free_lexer(lexer);
 	return (NULL);
+}
 
-	// * old stuff
-	// int				i;
-	// t_data_table	*table;
-	// 
-	// table = malloc(sizeof(t_data_table *));
-	// table->number_pip = count_pipes(str);
-	// table->list_of_commands = ft_split(str, '|');
-	// i = 0;
-	// while (table->list_of_commands[i] != NULL)
-	// {
-	// 	printf("%s\n", table->list_of_commands[i]);
-	// 	i++;
-	// }
-	return (NULL);
+void	free_lexer(t_lexer **lexer)
+{
+	int	i;
+
+	i = 0;
+	if (!lexer)
+		return ;
+	while (lexer[i] != NULL)
+	{
+		if (lexer[i] != NULL)
+		{
+			if (lexer[i]->args != NULL)
+				free(lexer[i]->args);
+			if (lexer[i]->cmd != NULL)
+				free(lexer[i]->cmd);
+			if (lexer[i]->flags != NULL)
+				free(lexer[i]->flags);
+			free(lexer[i]);
+		}
+		i++;
+	}
+	free(lexer);
 }
 
 int	count_char_until_next_token(char *input);
@@ -129,7 +133,7 @@ bool	is_a_token_id(char *input)
 	while (list_of_tokenid[i] != NULL)
 	{
 		j = 0;
-		while (list_of_tokenid[i][j] == input[j])
+		while (input[j] != '\0' && list_of_tokenid[i][j] == input[j])
 		{
 			j++;
 		}
@@ -144,27 +148,28 @@ bool	is_a_token_id(char *input)
 	return (false);
 }
 
-int copy_until_space(int i, char *input_after_curser, char **destination)
+int	copy_until_space(int i, char *input_after_curser, char **destination)
 {
 	int	j;
 
 	j = 0;
-	// puts("test new");
 	while (input_after_curser[i + j] != '\0')
 	{
-		if ((ft_isspace(input_after_curser[i + j]) || is_a_token_id(&input_after_curser[i + j])))
+		if ((ft_isspace(input_after_curser[i + j])
+				|| is_a_token_id(&input_after_curser[i + j])))
 			break ;
-		// puts("test");
-		printf("char index %d, is %c\n", j, input_after_curser[i + j]);
 		j++;
 	}
+	if (j == 0)
+		return (i + j);
 	(*destination) = (char *)malloc(sizeof(char) * (j + 2));
 	if (!(*destination))
 		exit(1);
 	j = 0;
 	while (input_after_curser[i + j] != '\0')
 	{
-		if ((ft_isspace(input_after_curser[i + j]) || is_a_token_id(&input_after_curser[i + j])))
+		if ((ft_isspace(input_after_curser[i + j])
+				|| is_a_token_id(&input_after_curser[i + j])))
 			break ;
 		(*destination)[j] = input_after_curser[i + j];
 		j++;
@@ -173,20 +178,19 @@ int copy_until_space(int i, char *input_after_curser, char **destination)
 	return (i + j);
 }
 
-int copy_until_tokenid(int i, char *input_after_curser, char **destination)
+int	copy_until_tokenid(int i, char *input_after_curser, char **destination)
 {
 	int	j;
 
 	j = 0;
-	// puts("test new");
 	while (input_after_curser[i + j] != '\0')
 	{
 		if (is_a_token_id(&input_after_curser[i + j]))
 			break ;
-		// puts("test");
-		printf("char index %d, is %c, next is null %d\n", i + j, input_after_curser[i + j], input_after_curser[i + j + 1] == '\0');
 		j++;
 	}
+	if (j == 0)
+		return (i + j);
 	(*destination) = (char *)malloc(sizeof(char) * (j + 2));
 	if (!(*destination))
 		exit(1);
@@ -199,7 +203,6 @@ int copy_until_tokenid(int i, char *input_after_curser, char **destination)
 		j++;
 	}
 	(*destination)[j] = '\0';
-	// puts("test");
 	return (i + j);
 }
 
@@ -208,7 +211,9 @@ int	write_the_right_token(int i, char *input_after_curser,char tokenid[3])
 	int	j;
 
 	j = 0;
-	while (input_after_curser[i + j] != '\0' && !ft_isspace(input_after_curser[i + j]) && is_a_token_id(&input_after_curser[i + j]))
+	while (input_after_curser[i + j] != '\0'
+		&& !ft_isspace(input_after_curser[i + j])
+		&& is_a_token_id(&input_after_curser[i + j]) && j < 2)
 	{
 		(tokenid)[j] = input_after_curser[i + j];
 		j++;
@@ -217,97 +222,83 @@ int	write_the_right_token(int i, char *input_after_curser,char tokenid[3])
 	return (i + j);
 }
 
-int parse_until_token_id(char *input, int current_lex, t_lexer **lexer, int curser)
+int parse_until_token_id(
+		char *input, int current_lex, t_lexer **lexer, int curser)
 {
-	int i;
-	bool function_done;
+	int		i;
+	bool	function_done;
 
-	puts("test");
 	i = 0;
 	function_done = false;
-	printf("input is %s, curent char %c\n\n", &input[i + curser], input[i + curser]);
 	while (input[i + curser] != '\0')
 	{
-		printf("doing char %d\n", i);
-		if (input[i + curser] == '-')
+		if (function_done && input[i + curser] == '-')
 			i = copy_until_space(i, &input[curser], &lexer[current_lex]->flags);
 		if (function_done == false && ft_isspace(input[i + curser]) == false)
 		{
-			puts("saving cmd");
 			i = copy_until_space(i, &input[curser], &lexer[current_lex]->cmd);
-			// puts("saved cmd");
-			puts(lexer[current_lex]->cmd);
 			function_done = true;
 			continue ;
 		}
 		if (function_done && ft_isspace(input[i + curser]) == false)
-		{
-			puts("function_done");
 			i = copy_until_tokenid(i, &input[curser], &lexer[current_lex]->args);
-		}
 		if (is_a_token_id(&input[i + curser]))
 		{
 			i = write_the_right_token(i, &input[curser], (lexer[current_lex]->tokenid));
 			break ;
 		}
-		
-		// printf("did parse %d\n", current_lex);
 		i++;
-		usleep(10000);
-		// printf("doing char %d, and next coninue %d\n", i, input[i + curser] != '\0');
-		// pause();
 	}
-	// puts("test");
 	return (curser + i);
 }
 
-int	copy_input_to_parcer_and_keep_track_of_curser(char *input, t_lexer *current_lex, int curser)
-{
-	int		i;
-	int		j;
-	int		k;
-	char	**list_of_tokenid;
+// int	copy_input_to_parcer_and_keep_track_of_curser(char *input, t_lexer *current_lex, int curser)
+// {
+// 	int		i;
+// 	int		j;
+// 	int		k;
+// 	char	**list_of_tokenid;
 
-	list_of_tokenid = get_list_of_tokenid();
-	i = 0;
-	current_lex->content = malloc(sizeof(char) * (count_char_until_next_token(&input[curser]) + 1));
-	// printf("content size is %d and curser is %d\n", count_char_until_next_token(&input[curser]), curser);
-	if (!current_lex->content)
-		exit(1);
-	while (input[curser + i] != '\0')
-	{
-		j = 0;
-		while (list_of_tokenid[j] != NULL)
-		{
-			k = 0;
-			while (list_of_tokenid[j][k] == input[curser + i + k])
-			{
-				k++;
-			}
-			if (list_of_tokenid[j][k] == '\0')
-			{
-				// puts("did it \n");
-				k = 0;
-				while (list_of_tokenid[j][k] != '\0')
-				{
-					current_lex->tokenid[k] = list_of_tokenid[j][k];
-					k++;
-				}
-				current_lex->tokenid[k] = '\0';
-				current_lex->content[i] = '\0';
-				free_list_of_tokenid(list_of_tokenid);
-				return (i + curser + k);
-			}
-			j++;
-		}
-		current_lex->content[i] = input[curser + i];
-		i++;
-	}
-	free_list_of_tokenid(list_of_tokenid);
-	current_lex->tokenid[0] = '\0';
-	current_lex->content[i] = '\0';
-	return(i + curser);
-}
+// 	list_of_tokenid = get_list_of_tokenid();
+// 	i = 0;
+// 	current_lex->content = malloc(sizeof(char) * (count_char_until_next_token(&input[curser]) + 1));
+// 	// printf("content size is %d and curser is %d\n", count_char_until_next_token(&input[curser]), curser);
+// 	if (!current_lex->content)
+// 		exit(1);
+// 	while (input[curser + i] != '\0')
+// 	{
+// 		j = 0;
+// 		while (list_of_tokenid[j] != NULL)
+// 		{
+// 			k = 0;
+// 			while (list_of_tokenid[j][k] == input[curser + i + k])
+// 			{
+// 				k++;
+// 			}
+// 			if (list_of_tokenid[j][k] == '\0')
+// 			{
+// 				// puts("did it \n");
+// 				k = 0;
+// 				while (list_of_tokenid[j][k] != '\0')
+// 				{
+// 					current_lex->tokenid[k] = list_of_tokenid[j][k];
+// 					k++;
+// 				}
+// 				current_lex->tokenid[k] = '\0';
+// 				current_lex->content[i] = '\0';
+// 				free_list_of_tokenid(list_of_tokenid);
+// 				return (i + curser + k);
+// 			}
+// 			j++;
+// 		}
+// 		current_lex->content[i] = input[curser + i];
+// 		i++;
+// 	}
+// 	free_list_of_tokenid(list_of_tokenid);
+// 	current_lex->tokenid[0] = '\0';
+// 	current_lex->content[i] = '\0';
+// 	return(i + curser);
+// }
 
 void	free_parser(t_lexer **lexer)
 {
@@ -318,13 +309,10 @@ void	free_parser(t_lexer **lexer)
 	i = 0;
 	while (lexer[i] != NULL)
 	{
-		// printf("address of lexer[i] == %p\nfreeing lexer %d content\n", lexer[i], i);
 		free(lexer[i]->content);
-		// printf("freeing lexer %d branch\n", i);
 		free(lexer[i]);
 		i++;
 	}
-	// printf("freeing lexer\n");
 	free(lexer);
 }
 
@@ -357,7 +345,7 @@ int	count_char_until_next_token(char *input)
 		i++;
 	}
 	free_list_of_tokenid(list_of_tokenid);
-	return(i);
+	return (i);
 }
 
 // I think this is lower then n^2 complexity, log(n) I think
@@ -378,7 +366,7 @@ int	count_token(char *input)
 		while (list_of_tokenid[j] != NULL)
 		{
 			k = 0;
-			while (list_of_tokenid[j][k] == input[i + k])
+			while (input[i + k] != '\0' &&list_of_tokenid[j][k] == input[i + k])
 			{
 				k++;
 			}
@@ -397,33 +385,38 @@ int	count_token(char *input)
 	return (num_of_token);
 }
 
-int	count_pipes(char *str)
-{
-	int	i;
-	int	number_of_pipes;
+// int	count_pipes(char *str)
+// {
+// 	int	i;
+// 	int	number_of_pipes;
 
-	i = 0;
-	number_of_pipes = 0;
-	while (str[i] != '\0')
-	{
-		if (str[i] == '|')
-			number_of_pipes++;
-		i++;
-	}
-	return (number_of_pipes);
-}
+// 	i = 0;
+// 	number_of_pipes = 0;
+// 	while (str[i] != '\0')
+// 	{
+// 		if (str[i] == '|')
+// 			number_of_pipes++;
+// 		i++;
+// 	}
+// 	return (number_of_pipes);
+// }
 
 char **get_list_of_tokenid(void)
 {
 	char **list_of_tokenid;
 
 	list_of_tokenid = (char **)malloc(sizeof(char *) * 7);
+	if (!list_of_tokenid)
+		exit(1);
 	list_of_tokenid[0] = ft_strdup("||");
 	list_of_tokenid[1] = ft_strdup("|");
 	list_of_tokenid[2] = ft_strdup("<<");
 	list_of_tokenid[3] = ft_strdup("<");
 	list_of_tokenid[4] = ft_strdup(">>");
 	list_of_tokenid[5] = ft_strdup(">");
+	if (!list_of_tokenid[0] || !list_of_tokenid[1] || !list_of_tokenid[2]
+		|| !list_of_tokenid[3] || !list_of_tokenid[4] || !list_of_tokenid[5])
+		exit(1);
 	list_of_tokenid[6] = NULL;
 	return (list_of_tokenid);
 }
